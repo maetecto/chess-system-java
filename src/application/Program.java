a911026bc8a5401fd65c52acaf4094f070764ced
maetecto <artur_paixao_de_matos@hotmail.com>
2021-08-04 13:42:19 +0100

    primeira classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("teste");
+		Position pos = new Position(1,2);
+		System.out.println(pos);
 	}
 
 }
